1bccf6f48755b39f1031af8eafead2bae163c057
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-27 00:21:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+@app.get("/")
+async def root():
+    return FileResponse("static/index.html")
 
 class LoginRequest(BaseModel):
     password: int
   
 @app.post("/api/answer")
 async def login(req: LoginRequest):
-    if req.password == "417":
+    if req.password == 417:
         return {"message": "クリア！写真撮って送りましょう(無理そうならなんか恥ずかしいセリフを言おう(？)"}
     else:
         return {"message": "パスワードが違います、当てずっぽうの場合はやめましょう"}
