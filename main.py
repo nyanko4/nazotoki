0f0f5def3b1b15bf194a9575b8124e6faefc04c3
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-27 00:07:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 class LoginRequest(BaseModel):
     password: int
   
-@app.post("/api/anser")
+@app.post("/api/answer")
 async def login(req: LoginRequest):
-    if req.password == 417:
+    if req.password == "417":
         return {"message": "クリア！写真撮って送りましょう(無理そうならなんか恥ずかしいセリフを言おう(？)"}
     else:
         return {"message": "パスワードが違います、当てずっぽうの場合はやめましょう"}
