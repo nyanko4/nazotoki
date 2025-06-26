926bcfba244d3f39d17a03890bfc00c5d6946d88
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-26 23:31:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static"), name="static")
+@app.get("/")
+async def read_index():
+    file_path = os.path.join(os.path.dirname(__file__), "index.html")
+    return FileResponse(file_path)
