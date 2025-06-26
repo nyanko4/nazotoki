9f665a75dd84b881bbc113fae6e80f01802903c1
nyanko4 <nyankobetsuaka@outlook.com>
2025-06-27 00:16:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 class LoginRequest(BaseModel):
-    password: str
+    password: int
   
 @app.post("/api/answer")
 async def login(req: LoginRequest):
